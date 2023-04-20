579f05430c5095c031d213b448013d28daadb375
Pablo Nieto Arias <104018391+PabloNietoA@users.noreply.github.com>
2023-04-20 13:51:39 +0200

    TestVariables

--- ProgAv-PECL/src/main/java/com/pablokarin/progav/pecl/ProgAvPECL.java
+++ ProgAv-PECL/src/main/java/com/pablokarin/progav/pecl/ProgAvPECL.java
@@ -4,13 +4,25 @@
 
 package com.pablokarin.progav.pecl;
 
+import com.pablokarin.progav.part1.hilos.Test;
+import java.util.concurrent.locks.*;
+
 /**
  *
  * @author Slend
  */
 public class ProgAvPECL {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    
+    
+    public static void main(String[] args) 
+    {
+        Lock l = new ReentrantLock();
+        Condition c = l.newCondition();
+        Test thread = new Test(1, l, c, true);
+        Test thread2 = new Test (2, l, c, false);
+        
+        thread.start();
+        thread2.start();
     }
 }
